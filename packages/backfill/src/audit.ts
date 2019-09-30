731286b3deef57029c70cf1bce36d84114f6af29
Benjamin Weggersen <bewegger@microsoft.com>
2019-09-30 11:03:57 +0200

    Revert "Use a temp file as a proxy to know when to close the watcher"
    This reverts commit 43268f221f082ce083fb2400b76347648a4d148a.

--- packages/backfill/src/audit.ts
+++ packages/backfill/src/audit.ts
@@ -1,19 +1,12 @@
 import * as chokidar from "chokidar";
 import * as findUp from "find-up";
 import * as path from "path";
-import * as fs from "fs-extra";
-import * as tempy from "tempy";
 import anymatch from "anymatch";
 import { logger } from "backfill-logger";
 import { WatchGlobs } from "backfill-config";
 
 let changedFilesOutsideScope: string[] = [];
 let changedFilesInsideScope: string[] = [];
-
-const END_AUDIT_FILEPATH = path.join(
-  tempy.directory(),
-  "END_AUDIT_FILENAME.txt"
-);
 
 let watcher: chokidar.FSWatcher;
 
@@ -72,17 +65,13 @@ export function initializeWatcher(
   const cacheFolderGlob = path.join("**", outputFolder, "**");
 
   watcher = chokidar
-    .watch([repositoryRoot, END_AUDIT_FILEPATH], {
+    .watch(repositoryRoot, {
       persistent: true,
       ignoreInitial: true,
       followSymlinks: false,
       ignored: ignoreGlobs
     })
     .on("all", (event, filePath) => {
-      if (filePath === END_AUDIT_FILEPATH) {
-        return;
-      }
-
       const logLine = `${filePath} (${event})`;
       logger.silly(`[audit] File change: ${logLine}`);
 
@@ -101,32 +90,16 @@ export const sideEffectCallToActionString =
 export const noSideEffectString =
   "[audit] All observed file changes were within the scope of the folder to be cached.";
 
-function showReportAndClose() {
-  if (changedFilesOutsideScope.length > 0) {
-    logger.warn(sideEffectWarningString);
-    changedFilesOutsideScope.forEach(file => logger.warn(`- ${file}`));
-    logger.warn(sideEffectCallToActionString);
-  } else {
-    logger.info(noSideEffectString);
-  }
-  watcher.close();
-}
-
-export async function closeWatcher() {
-  const waitForEndAudit = new Promise(resolve => {
-    const fallbackTimer = setTimeout(() => resolve(), 2500);
-
-    watcher.on("all", (_, filePath) => {
-      if (filePath === END_AUDIT_FILEPATH) {
-        clearTimeout(fallbackTimer);
-        resolve();
-      }
-    });
-
-    fs.writeFileSync(END_AUDIT_FILEPATH, "");
-  });
-
-  await waitForEndAudit;
-
-  showReportAndClose();
+export function closeWatcher() {
+  // Wait for one second before closing, giving time for file changes to propagate
+  setTimeout(() => {
+    if (changedFilesOutsideScope.length > 0) {
+      logger.warn(sideEffectWarningString);
+      changedFilesOutsideScope.forEach(file => logger.warn(`- ${file}`));
+      logger.warn(sideEffectCallToActionString);
+    } else {
+      logger.info(noSideEffectString);
+    }
+    watcher.close();
+  }, 1000);
 }
