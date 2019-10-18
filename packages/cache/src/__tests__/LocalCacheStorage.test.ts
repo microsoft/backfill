5dcfb4bb62a1379ba6998c7a049a4607db71d6d8
Benjamin Weggersen <bewegger@microsoft.com>
2019-10-18 16:07:18 +0200

    Add test for multiple output folders

--- packages/cache/src/__tests__/LocalCacheStorage.test.ts
+++ packages/cache/src/__tests__/LocalCacheStorage.test.ts
@@ -2,7 +2,7 @@ import * as fs from "fs-extra";
 import * as path from "path";
 
 import { setupFixture } from "backfill-utils-test";
-import { createDefaultConfig } from "backfill-config";
+import { createDefaultConfig, outputFolderAsArray } from "backfill-config";
 import { getCacheStorageProvider } from "../index";
 
 describe("LocalCacheStorage", () => {
@@ -24,7 +24,7 @@ describe("LocalCacheStorage", () => {
     const fetchFromCache = async (
       fixtureName: string,
       hash: string,
-      outputFolder: string
+      outputFolder: string | string[]
     ) => {
       // Setup
       const { cacheStorage, internalCacheFolder } = await setupCacheStorage(
@@ -32,15 +32,18 @@ describe("LocalCacheStorage", () => {
       );
 
       const secretFile = "qwerty";
-      const secretFileInCache = path.join(
-        internalCacheFolder,
-        hash,
-        outputFolder,
-        secretFile
-      );
 
-      //  Add file to the cache
-      await fs.outputFile(secretFileInCache, "");
+      outputFolderAsArray(outputFolder).forEach(folder => {
+        const secretFileInCache = path.join(
+          internalCacheFolder,
+          hash,
+          folder,
+          secretFile
+        );
+
+        //  Add file to the cache
+        fs.outputFileSync(secretFileInCache, "");
+      });
 
       // Execute
       const fetchResult = await cacheStorage.fetch(hash, outputFolder);
@@ -48,10 +51,12 @@ describe("LocalCacheStorage", () => {
       // Verify that fetch finished successfully
       expect(fetchResult).toBe(true);
 
-      // ... and that the secret file was copied over
-      const secretFileInDestination = path.join(outputFolder, secretFile);
-      const secretFileExists = await fs.pathExists(secretFileInDestination);
-      expect(secretFileExists).toBe(true);
+      outputFolderAsArray(outputFolder).forEach(folder => {
+        // ... and that the secret file was copied over
+        const secretFileInDestination = path.join(folder, secretFile);
+        const secretFileExists = fs.pathExistsSync(secretFileInDestination);
+        expect(secretFileExists).toBe(true);
+      });
     };
 
     it("will fetch on cache hit", async () => {
@@ -67,6 +72,14 @@ describe("LocalCacheStorage", () => {
         "any-location-with-cache",
         "46df1a257dfbde62b1e284f6382b20a49506f029",
         "dist"
+      );
+    });
+
+    it("will fetch on cache hit (multiple output folders: lib and dist)", async () => {
+      await fetchFromCache(
+        "multiple-output-folders-with-cache",
+        "46df1a257dfbde62b1e284f6382b20a49506f029",
+        ["lib", "dist"]
       );
     });
 
@@ -93,7 +106,7 @@ describe("LocalCacheStorage", () => {
     const putInCache = async (
       fixtureName: string,
       hash: string,
-      outputFolder: string
+      outputFolder: string | string[]
     ) => {
       // Setup
       const { cacheStorage, internalCacheFolder } = await setupCacheStorage(
@@ -101,24 +114,29 @@ describe("LocalCacheStorage", () => {
       );
 
       const secretFile = "qwerty";
-      const secretFileInDestination = path.join(outputFolder, secretFile);
 
-      // Add file to the cache
-      await fs.outputFile(secretFileInDestination, "");
+      outputFolderAsArray(outputFolder).forEach(folder => {
+        const secretFileInDestination = path.join(folder, secretFile);
+
+        // Add file to the cache
+        fs.outputFileSync(secretFileInDestination, "");
+      });
 
       // Execute
       await cacheStorage.put(hash, outputFolder);
 
       // Assert
-      const secretFileInCache = path.join(
-        internalCacheFolder,
-        hash,
-        outputFolder,
-        secretFile
-      );
+      outputFolderAsArray(outputFolder).forEach(folder => {
+        const secretFileInCache = path.join(
+          internalCacheFolder,
+          hash,
+          folder,
+          secretFile
+        );
 
-      const secretFileExists = await fs.pathExists(secretFileInCache);
-      expect(secretFileExists).toBe(true);
+        const secretFileExists = fs.pathExistsSync(secretFileInCache);
+        expect(secretFileExists).toBe(true);
+      });
     };
 
     it("will put cache in store", async () => {
@@ -137,6 +155,14 @@ describe("LocalCacheStorage", () => {
       );
     });
 
+    it("will put cache in store (multiple output folders: lib and dist)", async () => {
+      await putInCache(
+        "multiple-output-folders",
+        "46df1a257dfbde62b1e284f6382b20a49506f029",
+        ["lib", "dist"]
+      );
+    });
+
     it("will not persist cache when folder to cache does not exist", async () => {
       // Setup
       const { cacheStorage, internalCacheFolder } = await setupCacheStorage(
@@ -146,12 +172,11 @@ describe("LocalCacheStorage", () => {
       const outputFolder = "lib";
       const hash = "811c319a73f988d9260fbf3f1d30f0f447c2a194";
 
-      // Execute
+      // Execute and assert
       await expect(cacheStorage.put(hash, outputFolder)).rejects.toThrow(
         "Folder to cache does not exist"
       );
 
-      // Assert
       const cachedFolderExists = await fs.pathExists(internalCacheFolder);
       expect(cachedFolderExists).toBe(false);
     });
