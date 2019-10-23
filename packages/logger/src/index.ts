caded56ee6cd5b466f6dc6a3225a1bc361069217
Benjamin Weggersen <bewegger@microsoft.com>
2019-10-23 18:55:02 +0200

    Export config and make it possible to change log level via env variable (#108)

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,7 +1,7 @@
 import { logger as genericLogger } from "./genericLogger";
 import { performanceReportLogger } from "./performanceReportLogger";
 
-export { LogLevels, setLogLevel } from "./genericLogger";
+export { LogLevels, setLogLevel, isCorrectLogLevel } from "./genericLogger";
 
 export const logger = {
   ...genericLogger,
