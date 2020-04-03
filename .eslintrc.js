c7d0f34afda576fab033b992ef482b9b72e79845
Vincent Bailly <vibailly@tuta.io>
2020-04-03 09:56:23 +0200

    add prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
-  ]
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint"
+  ],
+  rules: {
+    "@typescript-eslint/no-var-requires": "off"
+  }
 };
